303e19b2158257d7f1a9b25f89728c0b928da2e8
micho137 <michaenrangelmr@gmail.com>
2022-10-21 18:37:06 -0500

    Seccion 6 y 7 terminadas

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import { AddCategory, GiftGrid } from "./components"
+
 export const GifExpertApp = () => {
+
+  const [categories, useCategories] = useState(['One Punch'])
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return
+    //useCategories([...categories,'Michaen'])
+    //useCategories(cat=>[...categories,'Michaen'])
+    useCategories([newCategory, ...categories])
+  }
+
   return (
     <>
-     <h1>GifExpertApp</h1>
+      <h1>GifExpertApp</h1>
+      <AddCategory
+        onNewCategory={onAddCategory}
+      />
+        {categories.map(category => {
+          return(
+            <GiftGrid key={category} category={category}/>
+          )
+        })}
     </>
   )
 }
